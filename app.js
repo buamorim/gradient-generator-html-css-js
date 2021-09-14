4bc3b41cbedb2833219f83d4a39ec163585cd198
buamorim <brunamorim.silva@outlook.com>
2021-09-14 16:43:53 -0300

    js started

---
+++ app.js
@@ -0,0 +1,4 @@
+let colorOne = document.getElementById('color-a');
+let colorTwo = document.getElementById('color-b');
+let currentDirection = 'to bottom';
+let outputCode = document.getElementById('code');
